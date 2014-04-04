474c7624b6c506a024bbf9c50833331b0aa4c8b7
NouraAlomar <nna2e13@soton.ac.uk>
2014-04-04 18:16:48 +0300

    created models

---
+++ server/test.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'lib/rb.phar';
+
+echo "hello world";
+
+R::setup('mysql:host=localhost;dbname=leapinit','root','');
+
+$person=R::dispense('person');
+
+$person->username = 'peter';
+$person->password='33333333333';
+$person->biography='KKKKKKK';
+
+
+
+
+$avatar=R::dispense('avatar');
+$avatar->facetype=1;
+$avatar->hairtype=2;
+$avatar->tietype=3;
+
+R::store($avatar);
+
+$person->avatar=$avatar;
+R::store($person);
+
+?>
