d0a93a91c5ddaa05cd155a816f533a3a8148757a
Peter West <peter.g.west@gmail.com>
2014-04-05 02:41:49 +0100

    basic styling done

---
+++ client/tests/all-screens.js
@@ -0,0 +1,53 @@
+function AllScreens ($scope) {
+	$scope.sizes = [
+		{ name: 'iPad - 1024x768', value: [768, 1024] },
+		{ name: 'mobile xlarge - 960x720', value: [720, 960] },
+		{ name: 'mobile large - 640x480', value: [480, 640] },
+		{ name: 'mobile normal - 470x320', value: [320, 470] },
+		{ name: 'mobile small - 426x320', value: [320, 426] }
+	];
+	_.clone($scope.sizes).forEach(function (size, i) {
+		$scope.sizes.splice(i * 2 + 1, 0, { name: size.name + ' (90deg)', value: [size.value[1], size.value[0]] });
+	});
+	$scope.size = $scope.sizes[6].value;
+
+	$scope.screens = ['start', 'register', 'interests', 'feed', 
+		'room', 'profile',  'avatar', 'wardrobe', 'scan', 'post', 'friends', 'rooms'];
+
+	$scope.people = [
+		{
+			username: 'CoolPlum90',
+			biography: 'Ignorance is bliss.',
+			medias: [ { size: 'half' }, { size: 'half' }, { size: 'full' }]
+		},
+		{
+			username: 'Dave',
+			biography: 'Blah.'
+		},
+		{
+			username: 'UniSoton',
+			biography: 'Blah.'
+		}
+	];
+
+	$scope.user = $scope.people[0];
+	$scope.user.friends = [$scope.people[1], $scope.people[2]];
+
+	$scope.error = {
+		message: 'Could not log you in.'
+	};
+
+	$scope.rooms = [
+		{ name: 'Careers' }
+	];
+
+	$scope.room = $scope.rooms[0];
+
+	$scope.user.rooms = [$scope.rooms[0]];
+
+	$scope.posts = [
+		{ person: $scope.people[1], room: $scope.rooms[0] },
+		{ person: $scope.people[0], room: $scope.rooms[0] }
+	]
+	$scope.post = $scope.posts[0];
+};
