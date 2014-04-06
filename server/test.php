44e5196951d9e9989180d23b54b97a66069018c4
NouraAlomar <nna2e13@soton.ac.uk>
2014-04-06 15:35:30 +0300

    additional models

--- server/test.php
+++ server/test.php
@@ -11,6 +11,10 @@ $person=R::dispense('person');
 $person->username = 'peter';
 $person->password='33333333333';
 $person->biography='KKKKKKK';
+$friends= array();
+$friends[]=$person;
+//$person->friends=$friends;
+
 
 
 
@@ -19,11 +23,82 @@ $avatar=R::dispense('avatar');
 $avatar->facetype=1;
 $avatar->hairtype=2;
 $avatar->tietype=3;
+$avatar->nosetype=2;
+$avatar->mouthtype=1;
+$avatar->eartype=9;
+$avatar->toptype=8;
+$avatar->accessory1Type=10;
+$avatar->accessory2Type=11;
+$avatar->eyetype=80;
+
+
 
 R::store($avatar);
 
 $person->avatar=$avatar;
-R::store($person);
 
+
+$room=R::dispense('room');
+$room->code='kk';
+$room->theme=1;
+$room->color=2;
+$room->size=11;
+$room->owner=$person;
+
+//$person->roomList[] = $room;
+R::store($room);
+
+
+$app=R::dispense('app');
+$app->loggedInPerson=$person;
+
+
+R::store($app);
+
+$sponsor=R::dispense('sponsor');
+$sponsor->paypalEmail='mmmmm@yahoo.com';
+$sponsor->paypalAuthcode='jkfgk';
+
+
+
+$sponsored_room=R::dispense('sponsored_room');
+$sponsored_room->owner=$sponsor;
+
+R::store($sponsor);
+R::store($sponsored_room);
+
+
+$media=R::dispense('media');
+$media->author='noura';
+
+
+$message=R::dispense('message');
+$message->to=$person;
+$message->media=$media;
+
+R::store($media);
+R::store($person);
+R::store($message);
+
+$audio=R::dispense('audio');
+$audio->path='kkkkkkkkk';
+R::store($audio);
+
+$picture=R::dispense('picture');
+$picture->path='iuihgff';
+R::store($picture);
+
+$video=R::dispense('video');
+$video->path='hjhggffghj';
+R::store($video);
+
+$text=R::dispense('text');
+$text->text='jhgkioop';
+R::store($text);
+
+$timeline=R::dispense('timeline');
+$timeline->orderBy='kkkjhg';
+
+R::store($timeline);
 
 ?>
