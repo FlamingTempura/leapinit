2519990ec2ec33a00b316244a8622b071026bae2
Peter West <peter.g.west@gmail.com>
2014-04-24 20:57:05 +0100

    scanning works :)

--- app/www/js/screens/scan.js
+++ app/www/js/screens/scan.js
@@ -2,27 +2,51 @@ angular.module('leapinit')
 	.controller('scanScreen', function ($rootScope, $scope, $location, models) {
 		var rooms = new models.Rooms();
 
-		$scope.submit = function () {
-			$scope.error = false;
-			$scope.loading = true;
-			if (!$scope.code) {
-				$scope.error = 'Please enter a code.';
-			}
-			if ($scope.error) {
-				$scope.loading = false;
-			} else {
-				rooms.fetchFromCode($scope.code).then(function (room) {
-					console.log('ROOM', room);
-					$rootScope.user.rooms.fetch().then(function () {
-						$rootScope.go('/room/' + room.id);
-						console.log('EH?')
-						$scope.$apply();
-					});
-				}).fail(function (response) {
-					$scope.error = response.responseJSON.msg;
+		if (typeof cordova !== 'undefined') {
+			cordova.plugins.barcodeScanner.scan(
+				function (result) {
+					alert("We got a barcode\n" +
+						"Result: " + result.text + "\n" +
+						"Format: " + result.format + "\n" +
+						"Cancelled: " + result.cancelled);
+					$scope.error = "Loading...";
+					$scope.$apply();
+					scan(result.text);
+				}, 
+				function (error) {
+					$scope.error = "Scanning failed";
+					$scope.$apply();
+				}
+			);
+		} else {
+			$scope.showForm = true;
+
+			$scope.submit = function () {
+				$scope.error = false;
+				if (!$scope.code) {
+					$scope.error = 'Please enter a code.';
+				}
+				if ($scope.error) {
 					$scope.loading = false;
+				} else {
+					scan($scope.code);
+				}
+			};
+		}
+
+		var scan = function (code) {
+			$scope.loading = true;
+			rooms.fetchFromCode(code).then(function (room) {
+				console.log('ROOM', room);
+				$rootScope.user.rooms.fetch().then(function () {
+					$rootScope.go('/room/' + room.id);
+					console.log('EH?')
 					$scope.$apply();
 				});
-			}
-		};
+			}).fail(function (response) {
+				$scope.error = response.responseJSON.msg;
+				$scope.loading = false;
+				$scope.$apply();
+			});
+		}
 	});
