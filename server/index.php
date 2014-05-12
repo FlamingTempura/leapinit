ba18847207199fd19cfca562a3acb11d967af957
Peter West <peter.g.west@gmail.com>
2014-05-12 01:59:43 +0000

    things

--- server/index.php
+++ server/index.php
@@ -25,18 +25,18 @@ $app = new \Slim\Slim();
 
 $params = json_decode($app->request->getBody());
 
-if (isset($_SERVER['HTTP_ORIGIN'])) {
+//if (isset($_SERVER['HTTP_ORIGIN'])) {
 	$app->response->headers->set('Access-Control-Allow-Origin', '*'); //$_SERVER['HTTP_ORIGIN']);
 	$app->response->headers->set('Access-Control-Allow-Credentials', true);
 	$app->response->headers->set('Access-Control-Max-Age', 86400);    // cache for 1 day
-}
+//}
 // Access-Control headers are received during OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+//if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+//	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
 		$app->response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 		$app->response->headers->set('Access-Control-Allow-Headers', $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
-	}
-}
+//	}
+//}
 
 $requestJSON = function () use (&$app) {
 	$app->view(new \JsonApiView());
